8192fb3098aa1260be6c819f43cf6e886144fd43
Gar <gar@lonelydatu.com>
2019-12-11 11:09:44 -0500

    one

---
+++ dev/proline_728x90/_js/main.js
@@ -0,0 +1,42 @@
+
+const start = ()=>{
+	const tl = new TimelineMax()
+	tl.set(".frame1", {opacity:1})
+	tl.from('.t1a', .01, {opacity:0}, "+=.4")
+	
+
+
+	const TIME = 6
+
+	const tlPlayer = new TimelineMax()
+	tlPlayer.from(".player_defend", TIME, {x:"-=15", y:"+=12", ease:Power3.easeOut}, 0)
+	tlPlayer.from(".player_run", TIME, {x:"+=15", y:"+=12", ease:Power3.easeOut}, 0)
+
+	tl.to('.frame1', .15, {opacity:0}, "+=1.6")
+
+	tl.add("f2")
+	tl.set(".frame2", {opacity:1})
+
+
+	tl.add("phone")
+	tl.from('.phone', .3, {y:"+=100"}, "phone")
+	tl.from('.illust', .3, {scale:0}, "phone+=.3")
+
+	tl.from('.t2', .01, {opacity:0}, "+=.2")
+	tl.from('.cta', .01, {opacity:0}, "+=.5")
+
+
+	
+
+	tl.from('.legal', .3, {opacity:0}, "+=1")
+
+	// tl.gotoAndPlay("f2")
+
+}
+
+start()
+
+
+
+module.exports = {};
+
