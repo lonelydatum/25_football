fbf70f6816b37e9e57da9f8f831f1e9a4847b6b6
Gar <gar@lonelydatu.com>
2019-12-11 10:17:02 -0500

    first commit

---
+++ dev/std_300x250/_js/main.js
@@ -0,0 +1,39 @@
+
+const start = ()=>{
+	const tl = new TimelineMax()
+	tl.set(".frame1", {opacity:1})
+	tl.from('.t1a', .01, {opacity:0}, "+=.2")
+	tl.from('.t1b', .01, {opacity:0}, "+=.6")
+
+
+	const TIME = 6
+
+	const tlPlayer = new TimelineMax()
+	tlPlayer.from(".player_defend", TIME, {x:"-=15", y:"+=22", ease:Power3.easeOut}, 0)
+	tlPlayer.from(".player_run", TIME, {x:"+=15", y:"+=22", ease:Power3.easeOut}, 0)
+
+	tl.to('.frame1', .2, {opacity:0}, "+=1.6")
+
+	tl.add("f2")
+	tl.set(".frame2", {opacity:1})
+
+	tl.from('.t2', .01, {opacity:0}, "+=.3")
+	tl.from('.cta', .01, {opacity:0}, "+=.5")
+
+
+	tl.add("phone")
+	tl.from('.phone', .3, {y:"+=100"}, "phone")
+	tl.from('.illust', .3, {scale:0}, "phone+=.3")
+
+	tl.from('.legal', .3, {opacity:0}, "+=1")
+
+	// tl.gotoAndPlay("f2")
+
+}
+
+start()
+
+
+
+module.exports = {};
+
